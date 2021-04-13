d8d9d872da83281548707b43d5fa5a008aed66e8
isama22 <emarti17@artic.edu>
2021-04-13 16:09:06 -0500

    styled blog page

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -16,6 +16,22 @@ function Bio() {
 
                 </div>
             </div>
+            <div className="user-info">
+                <div className="username">
+                    <p>isa</p>
+                </div>
+                <div className="username">
+                    <p>last seen: 1:01 pm 2/2/2021</p>
+                </div>
+                <div className="username">
+                    <p>posts: 22</p>
+                </div>
+            </div>
+            <div className="bio-text">
+                <p>
+                𝗜'𝗺 𝗯𝗮𝗯𝘆 𝗮𝘂𝘁𝗵𝗲𝗻𝘁𝗶𝗰 𝗮𝗿𝘁 𝗽𝗮𝗿𝘁𝘆 𝗵𝘂𝗺𝗯𝗹𝗲𝗯𝗿𝗮𝗴 𝗽𝗶𝗰𝗸𝗹𝗲𝗱 𝘀𝘂𝗯𝘄𝗮𝘆 𝘁𝗶𝗹𝗲 𝗸𝗶𝗰𝗸𝘀𝘁𝗮𝗿𝘁𝗲𝗿, 𝗲𝘁𝗵𝗶𝗰𝗮𝗹 𝗳𝗿𝗲𝗲𝗴𝗮𝗻 𝗽𝗼𝗿𝗸 𝗯𝗲𝗹𝗹𝘆 𝗷𝗲𝗮𝗻 𝘀𝗵𝗼𝗿𝘁𝘀 𝘁𝗿𝘆-𝗵𝗮𝗿𝗱. 𝗕𝘂𝘁𝗰𝗵𝗲𝗿 𝗫𝗢𝗫𝗢 𝗹𝗲𝗴𝗴𝗶𝗻𝗴𝘀, 𝘁𝘄𝗲𝗲 𝘆𝘂𝗰𝗰𝗶𝗲 𝘀𝗹𝗼𝘄-𝗰𝗮𝗿𝗯 𝘂𝗺𝗮𝗺𝗶 𝗺𝗮𝗻 𝗯𝘂𝗻 𝗬𝗢𝗟𝗢 𝗼𝗳𝗳𝗮𝗹 𝗺𝗲𝗴𝗴𝗶𝗻𝗴𝘀 𝗺𝗶𝘅𝘁𝗮𝗽𝗲 𝗲𝘁𝘀𝘆. 𝗺𝗲𝗵.
+                </p>
+            </div>
         </div>
 
     );
