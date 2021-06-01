0b57b5693b3e0a677418b8173b64c271304a7ad3
isama22 <emarti17@artic.edu>
2021-06-01 16:29:48 -0500

    initialized back end server

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from 'react'
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Projects from '../../pages/Projects/Projects'
@@ -5,19 +6,24 @@ import Blog from '../../pages/Blog/Blog'
 import Nav from '../../components/Nav/Nav'
 import Home from '../../pages/Home/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <div className='big-broider'>
-        <Nav />
+class App extends Component {
+
+  state = {}
+
+  render() {
+    return (
+      <div className="App">
+        <div className='big-broider'>
+          <Nav />
+        </div>
+        <Switch>
+          <Route path='/projects' render={() => (<Projects />)} />
+          <Route path='/blog' render={() => (<Blog />)} />
+          <Route path='/' render={() => (<Home />)} />
+        </Switch>
       </div>
-      <Switch>
-        <Route path='/projects' render={() => (<Projects />)} />
-        <Route path='/blog' render={() => (<Blog />)} />
-        <Route path='/' render={() => (<Home />)} />
-      </Switch>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
